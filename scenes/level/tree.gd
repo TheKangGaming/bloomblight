8400ba0fe704a8559d3cd89a31078bb04d564882
Kang <TheKangGaming@gmail.com>
2026-02-22 16:27:16 -0500

    Wood
    Trees give wood!

--- scenes/level/tree.gd
+++ scenes/level/tree.gd
@@ -1,5 +1,11 @@
 extends StaticBody2D
 
+var health = 4
+var is_stump = false
+
+@onready var sprite = $Sprite2D
+var stump_texture = preload("res://graphics/plants/trunk 1.png")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +17,34 @@ func _process(_delta: float) -> void:
 	pass
 
 func hit():
+	if is_stump:
+		return # Stop taking damage if it's already a stump!
+		
+	health -= 1
+	shake_tree()
+	
+	if health <= 0:
+		become_stump()
+
+func shake_tree():
+	# Create a quick code animation (Tween) to wobble the tree
 	var tween = create_tween()
-	tween.tween_property($Sprite2D.material, 'shader_parameter/progress', 1.0, 0.2)
-	tween.tween_property($Sprite2D.material, 'shader_parameter/progress', 0.0, 0.4)
+	var orig_x = sprite.position.x
+	
+	# Move left 4 pixels, right 4 pixels, then back to center rapidly
+	tween.tween_property(sprite, "position:x", orig_x - 4, 0.05)
+	tween.tween_property(sprite, "position:x", orig_x + 4, 0.1)
+	tween.tween_property(sprite, "position:x", orig_x, 0.05)
+
+func become_stump():
+	is_stump = true
+	sprite.texture = stump_texture
+	
+	# Optional: If the stump is shorter than the tree, you might need to move it down 
+	# so it sits on the crosshair properly!
+	# sprite.position.y += 10 
+	
+	# Give the player wood!
+	Global.inventory[Global.Items.WOOD] += 2
+	Global.inventory_updated.emit()
+	print("Tree chopped! Got 2 wood.")
