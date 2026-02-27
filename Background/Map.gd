17908a9a39ebac7ac028c79686669677ce318ce5
Kang <TheKangGaming@gmail.com>
2026-02-26 19:21:37 -0500

    Trying to fix it!

--- Background/Map.gd
+++ Background/Map.gd
@@ -1,19 +1,21 @@
 extends TileMapLayer
-var movement_data
+class_name Map
 
-func _ready():
-	## Reference variable to any particular movement cost of any particular tile
-	movement_data = tile_set.movement_data
+func _ready() -> void:
+	# We no longer need to look for the tutorial's custom tileset script
+	pass
 
-## Get the movement cost of any single cell on the map
-## We pass in the grid, so that we don't take in the data from tiles that have been placed outside the play area
-func get_movement_costs(grid: Grid):
-	var movement_costs = []
-	for y in range(grid.size.y):
-		movement_costs.append([])
-		for x in range(grid.size.x):
-			## This requires that all tiles with a movement cost MUST be on layer 0 of the tilemap
-			var tile = get_cell_source_id(Vector2i(x,y))
-			var movement_cost = movement_data.get(tile)
-			movement_costs[y].append(movement_cost)
-	return movement_costs
+func get_movement_costs() -> Dictionary:
+	var costs = {}
+	
+	# Get every single cell you painted on this layer
+	var used_cells = get_used_cells()
+	
+	for cell in used_cells:
+		# For right now, let's default all painted grass/dirt to a cost of 1
+		costs[cell] = 1
+		
+		# NOTE: Later, we can easily check your Epic_Tileset here to see 
+		# if a tile is a solid wall, and set its cost to 99 so Savannah can't pass!
+		
+	return costs
