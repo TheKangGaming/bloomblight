50969c5a3a82432e3a9266932cb89796f765ecfb
Kang <TheKangGaming@gmail.com>
2026-02-27 22:12:22 -0500

    Something's very broken

--- scenes/player/visuals.gd
+++ scenes/player/visuals.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
-# Catch the animation calls and pass them up to the parent (Player or Combat Unit)
-
+# 1. The moment the tool strikes the ground (The Middle Shout)
 func axe_use() -> void:
-	if get_parent().has_method("axe_use"):
-		get_parent().axe_use()
-
-func on_tool_animation_finished() -> void:
-	print("Visuals caught the animation!")
-	if get_parent().has_method("on_tool_animation_finished"):
-		get_parent()._on_tool_animation_finished()
-
-func _perform_tool_action() -> void:
-	if get_parent().has_method("perform_tool_action"):
+	if get_parent().has_method("_perform_tool_action"):
 		get_parent()._perform_tool_action()
+
+func hoe_use() -> void:
+	if get_parent().has_method("_perform_tool_action"):
+		get_parent()._perform_tool_action()
+
+func water_use() -> void:
+	if get_parent().has_method("_perform_tool_action"):
+		get_parent()._perform_tool_action()
+
+# 2. The moment the animation ends (The End Shout - THIS UNFREEZES YOU!)
+func _on_tool_animation_finished() -> void:
+	if get_parent().has_method("_on_tool_animation_finished"):
+		get_parent()._on_tool_animation_finished()
